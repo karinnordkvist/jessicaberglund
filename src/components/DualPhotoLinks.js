618af60679112872ca6337414940f5e2f7375b5a
Karin Nordkvist <kontakt@karinnordkvist.se>
2023-01-06 14:48:38 +0100

    Responsive changes. Netlify deploy 2

--- src/components/DualPhotoLinks.js
+++ src/components/DualPhotoLinks.js
@@ -9,14 +9,27 @@ const PhotoLinksWrapper = styled(FullWidthSection)`
   min-height: auto;
   padding: 50px 0;
 
+  div {
+    width: 50%;
+  }
+
   p {
     padding: 40px 60px 40px;
     line-height: 1.5;
   }
 
   img {
-    max-width: 50%;
+    max-width: 100%;
     padding: 0 50px;
+  }
+
+  h3 {
+    font-family: var(--font-secondary);
+    font-weight: 400;
+    font-size: 80px;
+    text-align: center;
+    transform: translateY(-50px);
+    color: #fff;
   }
 
   @media (max-width: 1200px) {
@@ -26,20 +39,44 @@ const PhotoLinksWrapper = styled(FullWidthSection)`
     img {
       padding: 20px;
     }
+
+    h3 {
+      transform: translateY(-80px);
+    }
+
+    @media (max-width: 768px) {
+      h3 {
+        transform: translateY(-66px);
+        font-size: 60px;
+      }
+    }
+
+    @media (max-width: 690px) {
+      h3 {
+        transform: translateY(-50px);
+        font-size: 40px;
+      }
+    }
   }
 `;
 
 const DualPhotoLinks = () => {
   return (
     <PhotoLinksWrapper>
-      <img
-        src="./images/311314965_1153875888877486_5513361764347484917_n.jpeg"
-        alt="Jessica stickar vid ett träd"
-      />
-      <img
-        src="./images/311314965_1153875888877486_5513361764347484917_n.jpeg"
-        alt="Jessica stickar vid ett träd"
-      />
+      <div>
+        <img
+          src="./images/313849679_573762377749840_447649959734290178_n.jpeg"
+          alt="Barn leker i höstlöven"
+        />
+        <h3>FOTO</h3>
+      </div>
+      <div>
+        <img
+          src="./images/312961694_937441233901670_5917840965797641745_n.jpeg"
+          alt="Jessica i dörröppning vid blomma"
+        />
+        <h3>FLORA</h3>
+      </div>
     </PhotoLinksWrapper>
   );
 };
